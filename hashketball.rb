f36be7868f33c49ce438b94563c6b9b6e921e8dc
Augustus Kirby <kirbyaugustus@gmail.com>
2019-04-19 16:11:22 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,57 @@
-# Write your code here!
-
-
-
-
-
-
-
-
-
-
+def game_hash
+  game_hash = {
+    :home => {
+      :team_name => ""
+        :colors => [""]
+        :players => {"names"}
+    },
+      :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
+    },
+      :hero => {
+        :name => "Romeo",
+        :age => "15"
+    },
+      :hero_friends => [
+        {
+           :name => "Benvolio",
+           :age => "17",
+           :attitude => "worried"
+        },
+        {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }
+      ]
+    },
+    :away => {
+      :patriarch => {
+        :name => "Lord Capulet",
+        :age => "50"
+      },
+      :matriarch => {
+        :name => "Lady Capulet",
+        :age => "51"
+      },
+      :heroine => {
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
+      },
+      :heroine_friends=> [
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }
+      ]
+    }
+  }
+  }
